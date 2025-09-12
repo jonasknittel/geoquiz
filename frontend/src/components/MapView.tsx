8f77d12471ba2c6aed40a730bbb83a9d7139ea5b
Jonas Knittel <jonasknittel2410@gmail.com>
2025-09-12 23:30:47 +0200

    geojson right directory

--- frontend/src/components/MapView.tsx
+++ frontend/src/components/MapView.tsx
@@ -1,19 +1,43 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { useEffect, useState } from 'react';
 
 export const MapView = () => {
+    const [geoData, setGeoData] = useState(null);
+
+    useEffect(() => {
+        fetch("/testgeojson/quiz_test.geojson")
+        .then((response) => response.json())
+        .then(
+            (data) => {
+                setGeoData(data)
+                console.log("GeoJSON geladen:", data)
+            }
+        )
+        .catch((err) => console.error("Failed to load GeoJSON:", err));
+    }, []);
+
     return (
         <div style={{ height: 400, width: '100%' }}>
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: '100%', width: '100%' }}>
+            <MapContainer   center={[47.5162, 14.5501]} // lat, lng
+                            zoom={7} 
+                            scrollWheelZoom={false} 
+                            style={{ height: '100%', width: '100%' }}
+                        >
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; OpenStreetMap contributors"
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+
+                {geoData && (
+                    <>
+                        <GeoJSON
+                            data={geoData}
+                            style={{ color: "black", fillColor: "lightblue", fillOpacity: 0.5 }}
+                            
+                        />
+                    </>
+                )}
             </MapContainer>
         </div>
     );
